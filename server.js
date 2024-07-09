d739524fd9001430f56444a3f36a21d55a5bed65
Adel <110723457+Addysd@users.noreply.github.com>
2024-07-10 01:07:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express();
 
 // Allow requests from Vercel deployment domain
 const corsOptions = {
-  origin: 'https://your-vercel-app.vercel.app',
+  origin: 'https://react-jobs-eta.vercel.app/',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
 
 // Example API route
-app.get('/api/jobs', (req, res) => {
+app.get('/https://json-server-7xnc.onrender.com/jobs/jobs', (req, res) => {
   // Example JSON response
   const jobs = [
     { id: 1, title: 'Software Engineer' },
