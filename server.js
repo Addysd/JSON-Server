3eb451f05978cef00592db1e40ab212eb69d37eb
Adel <110723457+Addysd@users.noreply.github.com>
2024-07-10 00:39:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const cors = require('cors');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('jobs.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(cors()); 
 server.use(router);
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
